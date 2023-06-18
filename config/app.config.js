bb0b05c91ff5a080a0f08f34538eb9bda5a541ea
Nyando Onongwene <nyandoonotex@gmail.com>
2023-06-18 12:49:53 +0100

    completed app config

---
+++ config/app.config.js
@@ -0,0 +1,10 @@
+require('dotenv').config;
+
+const MONGO_DB_CONFIG = {
+        DB: 'mongodb+srv://nyandoonotex:FLQDQl18S2skFIJC@reactbackendportfolio.tvbi3ro.mongodb.net/ReactBackendProject?retryWrites=true&w=majority'
+}
+
+
+module.exports = {
+    MONGO_DB_CONFIG
+}
